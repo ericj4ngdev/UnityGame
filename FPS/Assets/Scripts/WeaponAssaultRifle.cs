f1b5e78b43e42424c185e83b5aa33b35828b8182
ericjangdev_HomeDeskTop <ericjangdev@gmail.com>
2023-02-28 16:06:34 +0900

    Add Fire Effect and sound

--- FPS/Assets/Scripts/WeaponAssaultRifle.cs
+++ FPS/Assets/Scripts/WeaponAssaultRifle.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 
 public class WeaponAssaultRifle : MonoBehaviour
 {
+    [Header("Fire Effects")] [SerializeField]
+    private GameObject muzzleFlashEffect;           // 총구 이펙트
+    
     [Header("Audio Clips")] [SerializeField]
     private AudioClip audioClipTakeOutWeapon;       // 무기 장착 사운드
-
+    [SerializeField]
+    private AudioClip audioClipFire;
+    
     [Header("Weapon Setting")] [SerializeField]
     private WeaponSetting weaponSetting;            // 무기 설정
     private float lastAttackTime = 0;               // 마지막 발사시간 체크용
@@ -25,6 +30,8 @@ public class WeaponAssaultRifle : MonoBehaviour
     {
         // 무기장착 사운드 재생
         PlaySound(audioClipTakeOutWeapon);
+        // 총구 이펙트 오브젝트 비활성화
+        muzzleFlashEffect.SetActive(false);
     }
 
     public void StartWeaponAction(int type = 0)
@@ -72,9 +79,17 @@ public class WeaponAssaultRifle : MonoBehaviour
             lastAttackTime = Time.time;
             // 무기 애니메이션 재생
             animator.Play("Fire",-1,0); // 같은 애니메이션을 반복할 때, 애니메이션을 끊고 처음부터 다시 재생
+            StartCoroutine("OnMuzzleFlashEffect");
+            PlaySound(audioClipFire);
         }
     }
 
+    private IEnumerator OnMuzzleFlashEffect()
+    {
+        muzzleFlashEffect.SetActive(true);
+        yield return new WaitForSeconds(weaponSetting.attackRate * 0.3f);
+        muzzleFlashEffect.SetActive(false);
+    }
 
     private void PlaySound(AudioClip clip)
     {
